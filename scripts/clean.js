4aed68c2d69d4740f5b761cb62c652b7d0699c63
Jikku Jose <jikku@storybrain.com>
2022-10-26 17:39:47 +0530

    Remove query strings from URLS

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,10 +1,15 @@
 ;(function () {
   app.helpers.clean = text => {
-    return text.stripLinkPrefix()
+    return text.stripLinkPrefix().stripQueryString()
   }
 
   String.prototype.stripLinkPrefix = function () {
     const string = this
     return string.replace(/(https?:\/\/|www.)/g, "")
+  }
+
+  String.prototype.stripQueryString = function () {
+    const string = this
+    return string.replace(/\?.*$/, "")
   }
 })()
