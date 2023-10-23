6f7aea23ff2ed602407e958abf4214fd16ed9e5b
Jikku Jose <jikku@storybrain.com>
2023-10-23 21:04:23 +0530

    Add meta

---
+++ api/example.js
@@ -0,0 +1,22 @@
+import { kv } from "@vercel/kv"
+
+export default async function handler(req, res) {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  )
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  res.setHeader("Access-Control-Allow-Credentials", "true")
+
+  // Handle preflight requests (OPTIONS method)
+  if (req.method === "OPTIONS") {
+    res.status(204).end()
+    return
+  }
+
+  const meta = await kv.get("meta")
+
+  res.status(200).json({ meta })
+}
--- api/tskv.ts
+++ api/tskv.ts
@@ -1,16 +1,16 @@
-import { RequestContext } from "@vercel/edge"
-import kv from "@vercel/kv"
+import { kv } from "@vercel/kv"
+await kv.set("user_1_session", "session_token_value")
+const session = await kv.get("user_1_session")
 
-export const config = {
-  runtime: "edge",
-}
-
-export default async function handler(_: Request, ctx: RequestContext) {
-  const currentCount = (await kv.get<number>("visitor-count")) ?? 0
-  const incremented = currentCount + 1
-
-  // It doesn't seem to work properly on the dev server.
-  ctx.waitUntil(kv.set("visitor-count", incremented))
-
-  return new Response(`You're No. ${incremented} visitor!`)
+export default async function Cart({ params }) {
+  const cart = await kv.get(params.user)
+  return (
+    <div>
+      {cart?.map(item => (
+        <div key={item.id}>
+          {item.id} - {item.quantity}
+        </div>
+      ))}
+    </div>
+  )
 }
